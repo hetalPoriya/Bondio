7653681be12051d1178afd45271f9afbb5c55002
hetalporiya <byttezhetal@gmail.com>
2023-05-22 16:10:22 +0530

    commit

--- lib/screens/instagram_login.dart
+++ lib/screens/instagram_login.dart
@@ -1,6 +1,7 @@
 import 'package:bondio/controller/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
 class InstagramLogin extends StatelessWidget {
   const InstagramLogin({Key? key}) : super(key: key);
@@ -13,13 +14,16 @@ class InstagramLogin extends StatelessWidget {
       body: Container(
           height: double.infinity,
           width: double.infinity,
+          padding: paddingSymmetric(horizontalPad: 6.w),
           decoration: BoxDecoration(gradient: ColorConstant.linearColor),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               AppWidget.textFormFiledWhite(
                   textEditingController:
                       socialLoginController.instagramName.value,
                   hintText: 'Enter Instagram User Name'),
+              largeSizedBox,
               AppWidget.elevatedButton(
                   text: 'Continue',
                   onTap: () => socialLoginController.signInWithInstagram(
